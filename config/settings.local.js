0a4e59f3134fe4f6817646aa9ace7e9a75d70bd0
Justin Ware <wareisjustin@gmail.com>
2016-03-02 06:47:57 +0800

    Added env settings to build chain

--- config/settings.local.js
+++ config/settings.local.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
+   deployUser : 'NOT_APPLICABLE',
    deployDirectory: 'NOT_APPLICABLE'
 };
--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,17 @@ var colors = require('colors');
 var replace = require('gulp-replace');
 var print = require('gulp-print');
 var runSequence = require('run-sequence');
+var fs = require('fs');
 
 var config = require('./config/gulp.config');
 var envSettings = require('./config/settings');
 
-/* Clean Enviroment */
+/* Clean Build Artifacts */
 gulp.task('clean', function (cb) {
 
    del([config.paths.build, config.paths.temp]).then(function (paths) {
       var pathsText = paths.length === 0 ? 'NONE' : paths.join('; ');
-      console.log('[clean   ]'.yellow + ' Deleted: ' + pathsText);
+      console.log('[clean   ]'.yellow + ' Deleted: ' + pathsText.magenta);
       cb();
    });
 });
@@ -27,15 +28,28 @@ gulp.task('server-scripts', function() {
       //  .pipe(print(function(filepath) {
       //     return '[server-scripts]'.yellow + ' Prepared & copied: ' + filepath;
       //  }))
-       .pipe(replace('%APPNAME%', 'blabla'))
+       .pipe(replace('%APPNAME%', envSettings.appName))
+       .pipe(replace('%ENVIRONMENT%', envSettings.envName))
        .pipe(gulp.dest(config.paths.build + '/scripts'));
+});
+
+/* Prepare Environment settings */
+gulp.task('environment-settings', function (cb) {
+
+	//   var settingsContent = 'module.exports = {};';
+
+   //   fs.writeFile(config.paths.build + '/settings.js', settingsContent, function (err) {
+
+   //      cb();
+   //   });
+   cb();
 });
 
 /* Build entire solution */
 gulp.task('build', function (cb) {
 
    runSequence('clean',
-               ['server-scripts'],
+               ['server-scripts', 'environment-settings'],
                cb);
 });
 
