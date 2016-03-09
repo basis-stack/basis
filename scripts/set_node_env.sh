793111ad1dbc1d35db3ebec44e0cde5af062b942
justinware <justin@warebrained.com>
2016-03-09 04:33:38 +0000

    Dummy change to stage a travis build

--- scripts/set_node_env.sh
+++ scripts/set_node_env.sh
@@ -5,5 +5,7 @@ MSG_PREFIX="[SET_NODE_ENV] "
 # Set the runtime to a specific node version
 NODE_VERSION=%NODE_RUNTIME_ENV%
 echo $MSG_PREFIX"Setting node env to "$NODE_VERSION
+
+# TODO: Is this (running profile) the most efficient / best way to do this ?? Need to think this through
 . ~/.profile
 nvm use $NODE_VERSION
