441690200f908a873651a27b917dd7b99965f439
wareisjustin <wareisjustin@gmail.com>
2016-09-25 14:35:46 +0800

    Added http listen error handler + messages for known error types

---
+++ app/bin/errorHandler.js
@@ -0,0 +1,21 @@
+import logger from './../services/logger';
+
+export default (error, config, handledCallback) => {
+
+   const messagePrefix = '[SERVER ] LISTEN_ERROR:';
+
+   switch (error.code) {
+      case 'EACCES':
+         logger.error(`${messagePrefix} Port ${config.webServerPort} requires elevated privileges`);
+         break;
+      case 'EADDRINUSE':
+         logger.error(`${messagePrefix} Port ${config.webServerPort} is already in use`);
+         break;
+      default:
+         throw error;
+  }
+
+   if (handledCallback !== undefined) {
+      handledCallback();
+   }
+};
--- app/bin/startup.js
+++ app/bin/startup.js
@@ -1,17 +1,24 @@
 import config from './../services/config';
 import logger from './../services/logger';
 import app from './../app';
+import http from 'http';
+import handleListenError from './errorHandler';
 
 function onError(error) {
 
-   // Handle errors here
+   if (error.syscall !== 'listen') {
+      throw error;
+   }
+
+   handleListenError(error, config, () => { process.exit(1); });
 }
 
 function onListening() {
 
-   // Handle onListening status here
-
    logger.info(`[SERVER ] STARTED: listening on port ${config.webServerPort}`);
 }
 
-app.listen(config.webServerPort, onListening);
+const server = http.createServer(app);
+server.listen(config.webServerPort);
+server.on('error', onError);
+server.on('listening', onListening);
