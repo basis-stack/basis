e87061b60c3d87e1bf1053f5b6fd976a43f7681e
justinware <justin@warebrained.com>
2016-10-28 06:58:08 +0000

    npm package versions upgrade

--- config/gulp.config.js
+++ config/gulp.config.js
@@ -1,12 +1,10 @@
 const buildPath = './build';
-const tempPath = './temp';
 const appPath = './app';
 const packagePath = './package';
 
 export default {
    paths: {
       build: buildPath,
-      temp: tempPath,
       app: appPath,
       package: packagePath
    },
