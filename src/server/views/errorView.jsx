d40024b4aaa41caab67da3e3188ca687fc3b0601
wareisjustin <wareisjustin@gmail.com>
2017-08-15 04:57:50 +0000

    Fixed the layout of error pages, particularly long error messages

--- src/server/views/errorView.jsx
+++ src/server/views/errorView.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import HTTPStatus from 'http-status';
+import wordwrap from 'wordwrap';
 
 import Icon from './../components/icon';
 import material from './../components/materialContainer';
 
 const ErrorView = (props) => {
 
+  const wrap = wordwrap(100);
   const iconName = props.status === HTTPStatus.NOT_FOUND ? 'sentiment_dissatisfied' : 'error_outline';
+  const stackTrace = props.error.stack !== undefined ? <pre className="mdc-typography--body1 mdc-theme--text-hint-on-background">{ wrap(props.error.stack) }</pre> : null;
 
   return (
     <div className="basis-error-container">
       <div className="row middle-xs">
-        <div className="col-xs-offset-3 col-xs-1">
+        <div className="col-lg-offset-2 col-lg-1">
           <Icon value={iconName} size="48" />
         </div>
-        <div className="col-xs-8">
+        <div className="col-lg-6">
           <h1 className="mdc-typography--display1 mdc-theme--text-primary-on-background">{ props.status }. <small className="mdc-theme--text-secondary-on-background">{ props.statusText }</small></h1>
         </div>
+        <div className="col-lg-3">&nbsp;</div>
       </div>
       <div className="row">
-        <div className="col-xs-offset-4 col-xs-8">
+        <div className="col-lg-offset-3 col-lg-6">
           <h2 className="mdc-typography--subheading1 mdc-theme--text-primary-on-background">{ props.message }</h2>
-          <pre className="mdc-typography--body1 mdc-theme--text-hint-on-background">{ props.error.stack }</pre>
+          { stackTrace }
         </div>
+        <div className="col-lg-3">&nbsp;</div>
       </div>
     </div>
   );
