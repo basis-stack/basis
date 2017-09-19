57398e5f78ecbb56d2f3d5ee5f973023a34ac0db
wareisjustin <wareisjustin@gmail.com>
2017-09-19 03:25:09 +0000

    Added app version number (from package.json) to runtime config

--- config/settings.default.js
+++ config/settings.default.js
@@ -15,6 +15,7 @@ export default {
 
   // Client specific settings
   client: {
+    appDisplayName: 'Basis'
   },
 
   // Deploy settings
--- packages/server/src/core/config.js
+++ packages/server/src/core/config.js
@@ -1,8 +1,9 @@
 import jsonfile from 'jsonfile';
 import assignDeep from 'object-assign-deep';
+import path from 'path';
 
 import constants from './constants';
-import { getEnvVariable } from './utilities';
+import { getEnvVariable, getRootPath } from './utilities';
 
 export default class Config {
 
@@ -12,17 +13,24 @@ export default class Config {
           .forEach((prop) => { this[prop] = settings[prop]; });
   }
 
+  static getAppVersion() {
+
+    return jsonfile.readFileSync(path.join(getRootPath(), 'package.json'))
+                   .version;
+  };
+
   static createFromSettingsFile(filePath) {
 
     const defaultEnv = 'local';
     const runtimeEnv = getEnvVariable(constants.env.variables.node, defaultEnv);
     const allSettings = jsonfile.readFileSync(filePath);
+    const version = Config.getAppVersion();
 
     if (allSettings[runtimeEnv] === undefined) {
 
       throw new Error(`Unable to bootstrap for environment: '${runtimeEnv}'. No settings found in settings file for this environment.`);
     }
 
-    return new Config(assignDeep({}, allSettings.default, allSettings[runtimeEnv], { shared: { env: runtimeEnv } }));
+    return new Config(assignDeep({}, allSettings.default, allSettings[runtimeEnv], { shared: { env: runtimeEnv, version } }));
   }
 }
