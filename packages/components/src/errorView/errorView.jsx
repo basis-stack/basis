c4f1703a9435dc9bd7dfa66424a05f71bf05ceb2
wareisjustin <wareisjustin@gmail.com>
2017-10-06 08:06:27 +0000

    Pulled in react-mdc-web for Typography components

--- packages/components/src/errorView/errorView.jsx
+++ packages/components/src/errorView/errorView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HTTPStatus from 'http-status';
 import wordwrap from 'wordwrap';
+import { Caption, Display1, Subheading1 } from 'react-mdc-web/lib';
 
 import Icon from './../icon';
 import material from './../materialContainer';
@@ -9,7 +10,10 @@ const ErrorView = (props) => {
 
   const wrap = wordwrap(100);
   const iconName = props.status === HTTPStatus.NOT_FOUND ? 'sentiment_dissatisfied' : 'error_outline';
-  const stackTrace = props.error.stack !== undefined ? <pre className="mdc-typography--body1 mdc-theme--text-hint-on-background">{ wrap(props.error.stack) }</pre> : null;
+  const stackTrace = props.error.stack !== undefined ? (
+    <Caption component="pre" className="mdc-theme--text-secondary-on-background">
+      { wrap(props.error.stack) }
+    </Caption>) : null;
 
   return (
     <div className="basis-error-container">
@@ -18,13 +22,17 @@ const ErrorView = (props) => {
           <Icon value={iconName} size="48" />
         </div>
         <div className="col-lg-6">
-          <h1 className="mdc-typography--display1 mdc-theme--text-primary-on-background">{ props.status }. <small className="mdc-theme--text-secondary-on-background">{ props.statusText }</small></h1>
+          <Display1 component="h1" className="mdc-theme--text-primary-on-background">
+            { props.status }. <small className="mdc-theme--text-secondary-on-background">{ props.statusText }</small>
+          </Display1>
         </div>
         <div className="col-lg-3">&nbsp;</div>
       </div>
       <div className="row">
         <div className="col-lg-offset-3 col-lg-6">
-          <h2 className="mdc-typography--subheading1 mdc-theme--text-primary-on-background">{ props.message }</h2>
+          <Subheading1 component="h2" className="mdc-theme--text-primary-on-background">
+            { props.message }
+          </Subheading1>
           { stackTrace }
         </div>
         <div className="col-lg-3">&nbsp;</div>
