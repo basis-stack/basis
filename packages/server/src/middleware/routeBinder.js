3b15e09df22e95a2693607cd7f02814f249d65fe
wareisjustin <wareisjustin@gmail.com>
2018-03-15 04:52:10 +0000

    Made specific controller middleware additive (to the root controller middleware)

--- packages/server/src/middleware/routeBinder.js
+++ packages/server/src/middleware/routeBinder.js
@@ -3,25 +3,24 @@ import 'reflect-metadata';
 export default (controllerClass, controllerInstance, router) => {
 
   const baseRoute = Reflect.getMetadata('http:rootPath', controllerClass);
-  const baseMiddleware = Reflect.getMetadata('http:middleware', controllerClass);
+  const baseMiddleware = Reflect.hasMetadata('http:middleware', controllerClass) ?
+                           Reflect.getMetadata('http:middleware', controllerClass) : [];
   const methods = Object.getOwnPropertyNames(controllerClass.prototype)
                         .filter(m => m !== 'constructor' && Reflect.hasMetadata('http:path', controllerInstance, m));
 
   methods.forEach((name) => {
 
     const specificPath = Reflect.getMetadata('http:path', controllerInstance, name);
-    const specificMiddleware = Reflect.getMetadata('http:middleware', controllerInstance, name);
+    const specificMiddleware = Reflect.hasMetadata('http:middleware', controllerInstance, name) ?
+                                 Reflect.getMetadata('http:middleware', controllerInstance, name) : [];
     const httpMethod = Reflect.getMetadata('http:method', controllerInstance, name);
     const route = `${baseRoute}/${specificPath}`.replace('//', '/');
     const handler = controllerInstance[name].bind(controllerInstance);
 
     // TODO: Test this branch !!
+    if (baseMiddleware.length > 0 || specificMiddleware.length > 0) {
 
-    if (baseMiddleware !== undefined || specificMiddleware !== undefined) {
-
-      // TODO: Concatenate all specific middleware to base if it inddeed exists
-
-      const middleware = specificMiddleware !== undefined ? specificMiddleware : baseMiddleware;
+      const middleware = baseMiddleware.concat(specificMiddleware);
       router[httpMethod](route, ...middleware, handler);
     } else {
 
--- src/server/modules/home/index.js
+++ src/server/modules/home/index.js
@@ -2,8 +2,8 @@ import HomeController from './homeController';
 
 export default {
 
-  initRoutes: (router, container, passport) => {
+  initRoutes: (router, container) => {
 
-    HomeController.initialise(router, container, passport);
+    HomeController.initialise(router, container);
   }
 };
