098f80f26adf3f069ae7ab49a13c7d4a40d330fc
wareisjustin <wareisjustin@gmail.com>
2018-05-28 05:26:04 +0000

    Upgrade to @material 0.35. 1st cut of type defs for client & components packages

---
+++ packages/client/index.d.ts
@@ -0,0 +1,36 @@
+interface IClientModuleConfig {
+
+  key: string,
+
+  // TODO: Define proper types for these
+  route: any,
+  reducers: any
+}
+
+interface IClientModule {
+
+  // TODO: Define IClientModuleActions for this
+  actions: any;
+  initialise(param?: any): IClientModuleConfig;
+}
+
+declare module 'basis-client' {
+
+  // TODO: Declare a type (React Component / React Element) for app
+  export function initialise(modules: Array<IClientModuleConfig>, app: any): void;
+  export const routeTypes: {
+    page: string,
+    shellHub: string
+  }
+}
+
+declare module 'basis-client/modules' {
+
+  export const core: IClientModule;
+}
+
+declare module 'basis-client/components' {
+
+  // TODO: Declare this as a proper type (i.e. React.SFC<>)
+  export const RootRouter: any;
+}
---
+++ packages/components/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'basis-components' {
+
+  // TODO: Declare proper types for these (React.SFC<>)
+  export const Icon: any;
+  export const ErrorView: any;
+  export const material: any;
+}
