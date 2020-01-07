23440d0229c72024a1f33b1ee8f28f17dac05b5b
Justin Ware <wareisjustin@gmail.com>
2020-01-07 21:09:44 +1000

    Upgrade to react-router v5, react-redux v7, connected-react-router v6.6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,7 @@
 import { series, task } from 'gulp';
 import { initialiseTasks } from 'basis-build';
 
-const options = { logFileNames: true };
+const options = { logFileNames: true, lint: false };
 initialiseTasks(options);
 
 // Default gulp task
