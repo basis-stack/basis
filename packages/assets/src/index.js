a90265ad4625bf05128f99128deedf5d67913dd0
Justin Ware <wareisjustin@gmail.com>
2020-01-10 17:18:28 +1000

    1st cut of Material UI 4 upgrade

--- packages/assets/src/index.js
+++ packages/assets/src/index.js
@@ -7,7 +7,6 @@ export default {
   vendor: {
     fonts: [
       './node_modules/roboto-fontface/fonts/roboto/**/@(*Light|*Regular|*Medium).{eot,svg,ttf,woff,woff2}',
-      './node_modules/material-design-icons-iconfont/dist/fonts/**/*.{eot,svg,ttf,woff,woff2}',
       './node_modules/@fortawesome/fontawesome-free/webfonts/*.{eot,svg,ttf,woff,woff2}'
     ]
   }
